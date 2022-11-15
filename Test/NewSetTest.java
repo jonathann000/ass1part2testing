0ce5810dff29c3193e0bfa046d4b7282486d991c
jonathann000 <97891351+jonathann000@users.noreply.github.com>
2022-11-15 16:49:43 +0100

    Initial commit

---
+++ Test/NewSetTest.java
@@ -0,0 +1,110 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NewSetTest {
+    NewSet s;
+
+    public NewSetTest() {
+        s = new NewSet();
+    }
+
+    @BeforeEach
+    void setUp() {
+        s = new NewSet();
+    }
+
+    @AfterEach
+    void tearDown() {
+        Set s = new Set();
+    }
+
+    @Test
+    void toArray() {
+        s.insert(1);
+        s.insert(2);
+        s.insert(3);
+        int[] a = s.toArray();
+        assertEquals(3, a.length);
+        assertEquals(1, a[0]);
+        assertEquals(2, a[1]);
+        assertEquals(3, a[2]);
+    }
+
+    @Test
+    void insert() {
+        s.insert(1);
+        s.insert(2);
+        s.insert(3);
+        s.insert(0);
+        s.insert(2);
+        s.insert(-1);
+        int[] a = s.toArray();
+        assertEquals(5, a.length);
+        assertEquals(-1, a[0]);
+        assertEquals(0, a[1]);
+        assertEquals(1, a[2]);
+        assertEquals(2, a[3]);
+        assertEquals(3, a[4]);
+    }
+
+    @Test
+    void member() {
+        s.insert(1);
+        s.insert(2);
+        s.insert(3);
+        assertTrue(s.member(1));
+        assertTrue(s.member(2));
+        assertTrue(s.member(3));
+        assertFalse(s.member(4));
+    }
+
+    @Test
+    void intersect() {
+        s.insert(1);
+        s.insert(2);
+        s.insert(3);
+        s.insert(5);
+        NewSet s2 = new NewSet();
+        s2.insert(2);
+        s2.insert(3);
+        s2.insert(4);
+        s.intersect(s2);
+        int[] a = s.toArray();
+        assertEquals(2, a.length);
+        assertEquals(2, a[0]);
+        assertEquals(3, a[1]);
+    }
+
+
+    @Test
+    void distinctClosed() {
+        s.insert(1);
+        s.insert(2);
+        s.insert(3);
+        assertTrue(s.distinctClosed((a, b) -> a + b));
+        assertTrue(s.distinctClosed((a, b) -> a - b));
+        NewSet s2 = new NewSet();
+        s2.insert(1);
+        s2.insert(2);
+        s2.insert(-1);
+        assertTrue(s2.distinctClosed((a, b) -> a + b));
+        assertTrue(s2.distinctClosed((a, b) -> a - b));
+        NewSet s3 = new NewSet();
+        s3.insert(0);
+        s3.insert(1);
+        assertTrue(s3.distinctClosed((a,b) -> a+b));
+        assertTrue(s3.distinctClosed((a,b) -> a*b));
+        NewSet s4 = new NewSet();
+        s4.insert(1);
+        s4.insert(5);
+
+        assertFalse(s4.distinctClosed((a,b) -> a+b));
+        assertFalse(s4.distinctClosed((a,b) -> a-b));
+    }
+
+}
---
+++ src/NewSet.java
@@ -0,0 +1,84 @@
+import java.util.*;
+import java.util.function.IntBinaryOperator;
+
+public class NewSet {
+    private final ArrayList<Integer> a;
+
+    public NewSet() {
+        a = new ArrayList<Integer>();
+    }
+
+    public int[] toArray() {
+        int[] ia = new int[a.size()];
+        for (int i = 0; i < ia.length; i++) {
+            ia[i] = a.get(i);
+
+        }
+        return ia;
+    }
+
+    public void insert(int x) {
+        for (int i = 0; i < a.size(); i++) {
+            if (a.get(i) > x) {
+                a.add(i, x);
+                return;
+                // was break here
+            } else {
+                if (a.get(i) == x) {
+                    return;
+                   // was break here
+                }
+            }
+        }
+        a.add(x);
+    }
+
+    public boolean member(int x) {
+        for (int i = 0; i < a.size(); i++) {
+            if (a.get(i) > x) {
+                return false;
+            } else {
+                if (a.get(i) == x) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public void intersect(NewSet s) {
+        int i = 0;
+        for(int j = 0 ; i < a.size() && j < s.a.size();) {
+            if (a.get(i).equals(s.a.get(j))){
+                i++;
+                j++;
+            } else {
+                if (a.get(i) < s.a.get(j)) {
+                    a.remove(i);
+                    i++;
+                } else {
+                    j++;
+                }
+            }
+        }
+        for (int k = i; k < a.size();) {
+            a.remove(k);
+        }
+    }
+
+    // Try with:
+    //   (a, b) -> a + b;
+    //   (a, b) -> a - b;
+    public boolean distinctClosed(IntBinaryOperator f) {
+        int vi,vj;
+        for (int i = 0; i < a.size(); i++) {
+            for (int j = 0; j < a.size(); j++) {
+                vi = a.get(i);
+                vj = a.get(j);
+                //Check if intbinaryoperator is division
+                if ((member(f.applyAsInt(vi, vj)) && !(vi == vj))) return true;
+            }
+        }
+        return false;
+    }
+}
